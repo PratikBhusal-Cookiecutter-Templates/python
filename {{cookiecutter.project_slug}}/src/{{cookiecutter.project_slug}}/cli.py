db3c6e9686eef8ca81767ca0c655305011d6f093
PratikBhusal <PratikBhusal@users.noreply.github.com>
2020-06-27 21:10:09 -0500

    cli.py now conforms to black formatting

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -16,38 +16,18 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
-"""
-Copyright (c) 2013-2019, Ionel Cristian Mărieș
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-from typing import Tuple, Optional, Iterable
 {%- if cookiecutter.command_line_interface == 'Click' %}
+from typing import Tuple
+
 import click
 {%- elif cookiecutter.command_line_interface == 'Argparse' %}
 import argparse
+from typing import Optional, Sequence
 {%- endif %}
+
+
 {%- if cookiecutter.command_line_interface == 'Click' %}
+
 
 @click.command()
 @click.argument('args', nargs=-1)
@@ -56,11 +36,19 @@ def main(args: Tuple[str]) -> None:
 {%- elif cookiecutter.command_line_interface == 'Argparse' %}
 
 parser = argparse.ArgumentParser(description='My python package.')
-parser.add_argument('args', metavar='Arguments', nargs=argparse.ZERO_OR_MORE,
-                    help="Command line Arguments.")
+parser.add_argument(
+    'args',
+    metavar='Arguments',
+    nargs=argparse.ZERO_OR_MORE,
+    help="Command line Arguments.",
+)
 
 
-def main(args: Optional[Iterable[str]] = None) -> None:
+def main(args: Optional[Sequence[str]] = None) -> None:
     arguments = parser.parse_args(args=args)
     print(arguments.args)
 {%- endif %}
+
+
+if __name__ == "__main__":
+    main()
