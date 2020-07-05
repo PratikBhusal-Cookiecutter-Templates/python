ab9692f394d5fae4436ab7f18296205efe370bc4
PratikBhusal <PratikBhusal@users.noreply.github.com>
2020-07-05 14:13:19 -0500

    Add documentation option & utilize test fixtures

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,6 +1,16 @@
 # vim: set fileencoding=utf-8 :
 
 import pytest
+from typing import Tuple
 
-def test_example() -> None:
-    assert True
+
+@pytest.mark.parametrize(
+    "vals",
+    [
+        (True, True),
+        (False, False),
+    ],
+)
+def test_example(vals: Tuple[bool, bool]) -> None:
+    expected, actual = vals
+    assert expected == actual
